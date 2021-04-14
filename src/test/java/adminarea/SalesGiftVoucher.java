29ceb4c28d2f1d22567c962a26e7cda5dc8366b2
Evelina Markova <79656964+EvelinaMarkova@users.noreply.github.com>
2021-04-14 23:30:19 +0300

    final homework

---
+++ src/test/java/adminarea/SalesGiftVoucher.java
@@ -0,0 +1,37 @@
+package adminarea;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static adminarea.Browser.driver;
+import static org.openqa.selenium.By.cssSelector;
+
+public class SalesGiftVoucher {
+
+    public static void goToGiftVouchers(){
+        WebElement sales = driver.findElement(cssSelector("#menu-sale>a"));
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.elementToBeClickable(cssSelector("#menu-sale>a")));
+        sales.click();
+
+        WebElement giftVouchers2 = driver.findElement(cssSelector("#collapse4>li:nth-of-type(4)"));
+        wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.elementToBeClickable(cssSelector("#collapse4>li:nth-of-type(4)")));
+        giftVouchers2.click();
+
+        WebElement giftVouchers3 = driver.findElement(cssSelector(" #collapse4-3 >li:nth-of-type(1)"));
+        wait = new WebDriverWait(driver,15);
+        wait.until(ExpectedConditions.elementToBeClickable(cssSelector(" #collapse4-3 >li:nth-of-type(1)")));
+        giftVouchers3.click();
+
+        WebElement giftVouchers4 = driver.findElement(cssSelector(" #collapse4-3 >li:nth-of-type(1)"));
+        wait = new WebDriverWait(driver,15);
+        wait.until(ExpectedConditions.elementToBeClickable(cssSelector(" #collapse4-3 >li:nth-of-type(1)")));
+        giftVouchers4.click();
+
+
+
+    }
+}
+
---
+++ src/test/java/core/BaseTest.java
@@ -0,0 +1,19 @@
+package core;
+
+import adminarea.Browser;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+public class BaseTest {
+
+    @BeforeMethod
+    public void setup() {
+        Browser.setup();
+    }
+
+
+    @AfterMethod
+    public void quit() {
+        Browser.quit();
+    }
+
+}
